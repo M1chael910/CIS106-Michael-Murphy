5e08568bc4bcb143a5beb7a3a3705f36c2df1c24
M1chael910 <35551454+M1chael910@users.noreply.github.com>
2020-09-09 21:48:15 -0500

    Add files via upload

---
+++ Assignment 3/Activity1.js
@@ -0,0 +1,15 @@
+function main() {
+    // This program prompts the user for their hours and rate per hour and then calculates the monthly, weekly, and yearly pay of the user
+    var weeklyPay;
+    var monthlyPay;
+    var yearlyPay;
+    
+    window.alert("Enter hourly rate");
+    hourlyRate = window.prompt('Enter a value for hourlyRate');
+    window.alert("Enter Hours");
+    Hours = window.prompt('Enter a value for Hours');
+    weeklyPay = hourlyRate * Hours;
+    monthlyPay = weeklyPay * 4;
+    yearlyPay = monthlyPay * 12;
+    window.alert(weeklyPay.ToString() + monthlyPay + yearlyPay);
+}
