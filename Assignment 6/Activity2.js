6c178aa86d66a12717d9d15fd64b21dc67b097dd
M1chael910 <35551454+M1chael910@users.noreply.github.com>
2020-09-30 10:52:35 -0500

    Update Activity2.js

--- Assignment 6/Activity2.js
+++ Assignment 6/Activity2.js
@@ -4,72 +4,71 @@
 
 main();
 
-function main() { 
-    var age = getAge();
-    var ageInMonths = calculateAgeInMonths(age);
-    var ageInDays = calculateAgeInDays(age);
-    var ageInHours = calculateAgeInHours(age);
-    var ageInSeconds = calculateAgeInSeconds(age);
-    outputAge(age,ageInMonths,ageInDays,ageInHours,ageInSeconds);
+function main() {
+	var age = getAge();
+	var ageInMonths = calculateAgeInMonths(age);
+	var ageInDays = calculateAgeInDays(age);
+	var ageInHours = calculateAgeInHours(age);
+	var ageInSeconds = calculateAgeInSeconds(age);
+	outputAge(age, ageInMonths, ageInDays, ageInHours, ageInSeconds);
 }
 
 
-function getAge() { 
-    var ageInYears = input("Enter Age in years");
-    return ageInYears;
+function getAge() {
+	var ageInYears = input("Enter Age in years");
+	return ageInYears;
 }
 
-function calculateAgeInMonths(age) { 
-    var ageInMonths = age * 12;
-    return ageInMonths;
-}
-function calculateAgeInDays(age) { 
-    var ageInDays = age * 365;
-    return ageInDays;
-}
-function calculateAgeInHours(age) { 
-    var ageInHours = age * 8760;
-    return ageInHours;
-}
-function calculateAgeInSeconds(age) { 
-    var ageInSeconds = age * 31557600;
-    return ageInSeconds;
+function calculateAgeInMonths(age) {
+	var ageInMonths = age * 12;
+	return ageInMonths;
 }
 
-function outputAge(ageInYears,ageInMonths,ageInDays,ageInHours,ageInSeconds) { 
-    output(ageInYears + " years is " 
-    + ageInMonths + " months," 
-    + ageInDays + " days, " + 
-    ageInHours + " hours, and " + 
-    ageInSeconds + " seconds");
+function calculateAgeInDays(age) {
+	var ageInDays = age * 365;
+	return ageInDays;
+}
+
+function calculateAgeInHours(age) {
+	var ageInHours = age * 8760;
+	return ageInHours;
+}
+
+function calculateAgeInSeconds(age) {
+	var ageInSeconds = age * 31557600;
+	return ageInSeconds;
+}
+
+function outputAge(ageInYears, ageInMonths, ageInDays, ageInHours, ageInSeconds) {
+	output(ageInYears + " years is " +
+		ageInMonths + " months," +
+		ageInDays + " days, " +
+		ageInHours + " hours, and " +
+		ageInSeconds + " seconds");
 }
 
 function input(text) {
-  if (typeof window === 'object') {
-    return prompt(text)
-  }
-  else if (typeof console === 'object') {
-    const rls = require('readline-sync');
-    var value = rls.question(text);
-    return value;
-  }
-  else {
-    output(text);
-    var isr = new java.io.InputStreamReader(java.lang.System.in); 
-    var br = new java.io.BufferedReader(isr); 
-    var line = br.readLine();
-    return line.trim();
-  }
+	if (typeof window === 'object') {
+		return prompt(text)
+	} else if (typeof console === 'object') {
+		const rls = require('readline-sync');
+		var value = rls.question(text);
+		return value;
+	} else {
+		output(text);
+		var isr = new java.io.InputStreamReader(java.lang.System.in);
+		var br = new java.io.BufferedReader(isr);
+		var line = br.readLine();
+		return line.trim();
+	}
 }
 
 function output(text) {
-  if (typeof document === 'object') {
-    document.write(text);
-  } 
-  else if (typeof console === 'object') {
-    console.log(text);
-  } 
-  else {
-    print(text);
-  }
+	if (typeof document === 'object') {
+		document.write(text);
+	} else if (typeof console === 'object') {
+		console.log(text);
+	} else {
+		print(text);
+	}
 }
