7fedc4a96595b4e320f9416e86b7a33b37f5ea83
M1chael910 <35551454+M1chael910@users.noreply.github.com>
2020-12-03 17:46:41 -0600

    Update Activity1.js

--- Assignment 14/Activity1.js
+++ Assignment 14/Activity1.js
@@ -1,46 +1,48 @@
-// This program pulls numbers out the file "scores.txt" and 
-// calculates and outputs the minumum, maximum, and average.
+// This program pulls numbers out the file "scores.txt" and
+// puts them in an array, and calculates and outputs the 
+// minumum, maximum, and average.
 
 main();
-function main() { 
+
+function main() {
     var filename = "scores.txt";
-    if (fileExists(filename)) { 
-        var file = getFile(filename);
-        var scoreArray = getScoreArray(file);
-        output(scoreArray);
-        var min = calculateMin(scoreArray);
-        var max = calculateMax(scoreArray);
-        var average = calculateAverage(scoreArray);
-        output("The minimum is "+ min + ", the maximum is " + max + ", and the average is " + average + ".");
-    } else { 
-        output("File not found");
+    var file = getFile(filename);
+    var scoreArray = getScoreArray(file);
+    output(scoreArray);
+    var min = calculateMin(scoreArray);
+    var max = calculateMax(scoreArray);
+    var average = calculateAverage(scoreArray);
+    output("The minimum is " + min + ", the maximum is " + max + ", and the average is " + average + ".");
+}
+
+function getFile(filename) {
+    var fs = require('fs');
+    var exists = fs.existsSync(filename);
+    if (!exists) {
+        output("Missing file: " + filename);
+        process.exit(1);
+    } else {
+        var file = fs.readFileSync(filename, 'utf8');
+        return file;
     }
 }
-function getFile(filename) {
-    var file = require('readline').createInterface( {
-      input: require('fs').createReadStream(filename)
-    });
-    return file;
-}
-function getScoreArray(file) { 
-    var scores = [];
-    file.on('line', function (line) {
-        output(line);
-        var scoreString = line.slice(line.indexOf(",")+1,line.length);
-        var scoreInt = parseInt(scoreString);
-        output(scoreInt);
-        scores.push(scoreInt);
-    });
-    return scores;
-}
 
-function fileExists(filename) {
-    var fs = require('fs');
-    return fs.existsSync(filename);
+function getScoreArray(file) {
+    var scoreArray = [];
+    var lines = file.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var scoreInt = parseInt(line.slice(line.indexOf(",")+1,line.length));
+        scoreArray.push(scoreInt);
+    }
+    return scoreArray;
 }
-
 function calculateAverage(scores) {
-    var average = scores / scores.length;
+    var total = 0;
+    for(var i = 0; i < scores.length; i++) {
+        total += scores[i]; 
+    }
+    var average = total / scores.length;
     return average;
 }
 function calculateMax(scores) {
@@ -56,29 +58,12 @@ function calculateMax(scores) {
 function calculateMin(scores) {
     var min = scores[0];
     for (var i = 1; i < scores.length; i++) {
-            if (min < scores[i]) { 
-                min = scores[i];
+        if (min < scores[i]) {
+            min = scores[i];
         }
     }
     return min;
 }
-
-function input(text) {
-    if (typeof window === 'object') {
-        return prompt(text)
-    } else if (typeof console === 'object') {
-        const rls = require('readline-sync');
-        var value = rls.question(text);
-        return value;
-    } else {
-        output(text);
-        var isr = new java.io.InputStreamReader(java.lang.System.in);
-        var br = new java.io.BufferedReader(isr);
-        var line = br.readLine();
-        return line.trim();
-    }
-}
-
 function output(text) {
     if (typeof document === 'object') {
         document.write(text);
@@ -88,3 +73,4 @@ function output(text) {
         print(text);
     }
 }
+
