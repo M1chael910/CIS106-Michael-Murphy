7e734243b2d8f64a9c23fa9257ffdf6147edf082
M1chael910 <35551454+M1chael910@users.noreply.github.com>
2020-12-03 22:40:52 -0600

    Add files via upload

---
+++ Assignment 14/Activity2.js
@@ -0,0 +1,89 @@
+// This program pulls numbers out the file "scores.txt" and
+// puts them in an array, and calculates and outputs the 
+// minumum, maximum, and average.
+
+const { exit } = require('process');
+
+main();
+
+function main() {
+    var filename = "scores.txt";
+    var file = getFile(filename);
+    var scoreArray = getScoreArray(file);
+    output(scoreArray);
+    var min = calculateMin(scoreArray);
+    var max = calculateMax(scoreArray);
+    var average = calculateAverage(scoreArray).toFixed(2);
+    output("The minimum is " + min + ", the maximum is " + max + ", and the average is " + average + ".");
+}
+
+function getFile(filename) {
+    var fs = require('fs');
+    var exists = fs.existsSync(filename);
+    if (!exists) {
+        output("Missing file: " + filename);
+        process.exit(1);
+    } else {
+        var file = fs.readFileSync(filename, 'utf8');
+        return file;
+    }
+}
+
+function getScoreArray(file) {
+    var scoreArray = [];
+    var lines = file.split("\n");
+    for (var i = 1; i < lines.length; i++) {
+        var line = lines[i];
+        var indexOfComma = line.indexOf(",");
+        if (indexOfComma == -1) {
+            output("Incorrect Format");
+            process.exit(1);
+        } else { 
+            var scoreInt = parseInt(line.slice(indexOfComma+1,line.length));
+            if (scoreInt == NaN) { 
+                output("Data is incorrect");
+                process.exit(1);
+            } else { 
+                scoreArray.push(scoreInt);
+            }
+        }
+    }
+    return scoreArray;
+}
+function calculateAverage(scores) {
+    var total = 0;
+    for(var i = 0; i < scores.length; i++) {
+        total += scores[i]; 
+    }
+    var average = total / scores.length;
+    return average;
+}
+function calculateMax(scores) {
+    var max = scores[0];
+    for (var i = 0; i < scores.length; i++) {
+        if (max > scores[i]) {
+            max = scores[i];
+        }
+    }
+    return max;
+}
+
+function calculateMin(scores) {
+    var min = scores[0];
+    for (var i = 1; i < scores.length; i++) {
+        if (min < scores[i]) {
+            min = scores[i];
+        }
+    }
+    return min;
+}
+function output(text) {
+    if (typeof document === 'object') {
+        document.write(text);
+    } else if (typeof console === 'object') {
+        console.log(text);
+    } else {
+        print(text);
+    }
+}
+
