adeb4e6c57ffd8fda6fac1e38b554145bbb77430
M1chael910 <mpmurphybusiness@gmail.com>
2020-12-07 23:06:52 -0600

    Created function to create the arrays

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -9,21 +9,27 @@ function loadFileData() {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     let fileUrl = "https://www.w3schools.com/xml/plant_catalog.xml";
     var req = new XMLHttpRequest();
+    
     req.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-      parseData(this);
+        parseData(this.responseText);
       }
     };
     req.open("GET", fileUrl, true);
     req.send();
 }
 
-function parseData(xml) { 
-    output(xml);
-    // var x = doc.getElementsByTagName("PLANT");
-    // output(x);
-    // var element = doc.getElementsByTagName("PLANT");
-    // output(element); 
+function createArrays(xmlText) {;
+    var lines = xmlText.split("\n");
+    // 288
+    for (var i = 2; i < lines.length - 2; i++) { 
+        var line = lines[i];
+        if (line.includes("<COMMON>")) { 
+            var 
+        }
+        output(lines[i]);
+    }
+    output(lines.length);
 }
 
 
