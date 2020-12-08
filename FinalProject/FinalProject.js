dd6133efa7e4fb07b81eac1d4ac30b3faa720a87
M1chael910 <mpmurphybusiness@gmail.com>
2020-12-07 20:00:59 -0600

    Created function to pull XML data from Url

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -2,5 +2,39 @@
 
 main();
 function main() { 
+    loadFileData();
+}
 
+function loadFileData() {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    let fileUrl = "https://www.w3schools.com/xml/plant_catalog.xml";
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      parseData(this);
+      }
+    };
+    req.open("GET", fileUrl, true);
+    req.send();
+}
+
+function parseData(xml) { 
+    output(xml);
+    // var x = doc.getElementsByTagName("PLANT");
+    // output(x);
+    // var element = doc.getElementsByTagName("PLANT");
+    // output(element); 
+}
+
+
+
+
+function output(text) {
+    if (typeof document === 'object') {
+        document.write(text);
+    } else if (typeof console === 'object') {
+        console.log(text);
+    } else {
+        print(text);
+    }
 }
